e9d7b8f319668dfba4bde0d962bb01a80b05c837
dyyyl <hey@dyyyl.rocks>
2021-02-17 18:33:51 +0100

    update test to account for react query

--- src/__tests__/index.test.tsx
+++ src/__tests__/index.test.tsx
@@ -2,10 +2,17 @@ import { render, screen } from '@testing-library/react';
 import React from 'react';
 
 import IndexPage from 'pages/index';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+const queryClient = new QueryClient();
 
 describe('Index Page', () => {
   test('render a welcome message', () => {
-    render(<IndexPage />);
+    render(
+      <QueryClientProvider client={queryClient}>
+        <IndexPage />
+      </QueryClientProvider>,
+    );
 
     const title = screen.getByText(/Hello, all!/i);
 
