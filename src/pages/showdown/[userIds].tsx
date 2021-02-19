52d4fe487b92813170d212200e2bc59b15013fc2
dyyyl <hey@dyyyl.rocks>
2021-02-19 15:11:11 +0100

    clean up client data loading

--- src/pages/showdown/[userIds].tsx
+++ src/pages/showdown/[userIds].tsx
@@ -1,60 +1,43 @@
 import { NextPage } from 'next';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-import React, { useState } from 'react';
-// import { QueryClient } from 'react-query';
-// import { dehydrate, DehydratedState } from 'react-query/hydration';
+import React from 'react';
 
 import { Main, Section } from 'shared/components/Containers';
 import { Big, Title } from 'shared/components/Typography';
 import User from 'shared/components/User';
 
+import useUser from 'shared/hooks/useUser';
+
 const UserShowdownPage: NextPage = () => {
-  const [
-    firstUserFollowerCount,
-    setFirstUserFollowerCount,
-  ] = useState<number>();
-
-  const [
-    secondUserFollowerCount,
-    setSecondUserFollowerCount,
-  ] = useState<number>();
-
   const router = useRouter();
 
   const { userIds } = router.query;
 
   const [firstUserId, secondUserId] = String(userIds).split('-vs-');
 
+  const { data: firstUserData, isLoading: firstUserIsLoading } = useUser(
+    firstUserId,
+  );
+
+  const { data: secondUserData, isLoading: secondUserIsLoading } = useUser(
+    secondUserId,
+  );
+
   return (
     <Main>
-      {firstUserFollowerCount && secondUserFollowerCount ? (
+      {firstUserData?.followers.totalCount &&
+      secondUserData?.followers.totalCount ? (
         <Title>NOT EVEN CLOSE</Title>
       ) : (
         <Title>WHO WILL WIN?</Title>
       )}
       <Section>
-        <User
-          userId={firstUserId}
-          setFollowerCount={setFirstUserFollowerCount}
-          win={
-            firstUserFollowerCount &&
-            secondUserFollowerCount &&
-            firstUserFollowerCount > secondUserFollowerCount
-          }
-        />
+        <User user={firstUserData} isLoading={firstUserIsLoading} />
 
         <Big>VS.</Big>
 
-        <User
-          userId={secondUserId}
-          setFollowerCount={setSecondUserFollowerCount}
-          win={
-            firstUserFollowerCount &&
-            secondUserFollowerCount &&
-            secondUserFollowerCount > firstUserFollowerCount
-          }
-        />
+        <User user={secondUserData} isLoading={secondUserIsLoading} />
       </Section>
 
       <Link href="/">
@@ -63,19 +46,5 @@ const UserShowdownPage: NextPage = () => {
     </Main>
   );
 };
-
-// export async function getStaticProps(): Promise<
-//   Record<string, Record<string, DehydratedState>>
-// > {
-//   const queryClient = new QueryClient();
-
-//   await queryClient.prefetchQuery(['user', userId], () => getUser(userId));
-
-//   return {
-//     props: {
-//       dehydratedState: dehydrate(queryClient),
-//     },
-//   };
-// }
 
 export default UserShowdownPage;
